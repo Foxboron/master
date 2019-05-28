8c95554706c18b5ae4ec9f59a81486a933b7c97b
Morten Linderud <morten@linderud.pw>
2019-05-28 20:07:14 +0200

    Added stats

---
+++ stats/plot_package_count.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv("stats.txt", sep=" ")
+
+f = plt.figure()
+
+data["Leafs"].plot()
+data["Level"].plot()
+data["Total"].plot()
+plt.margins(0)
+plt.legend()
+plt.show()
+f.savefig("nodes_count_over_time.pdf", bbox_inches='tight')
---
+++ stats/plot_response_time.py
@@ -0,0 +1,17 @@
+import math
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv("stats.txt", sep=" ")
+
+f = plt.figure()
+
+data["Duration"].plot(label="Response time")
+data["Duration"].rolling(700, min_periods=0).mean().plot(label="Mean over time")
+plt.title("Response time")
+plt.ylabel("Seconds")
+plt.xlabel("Number of Entries")
+
+plt.margins(0)
+plt.legend()
+plt.show()
+f.savefig("foo.pdf", bbox_inches='tight')
